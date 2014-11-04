2f7313ead1d7f1a13fed7cffb1176b5136057370
alexsayal <alexandresayal@gmail.com>
2014-11-04 22:43:06 +0200

    Organisation Added option to choose query tweet in task2 Added time elapsed in task2

--- src/Main.java
+++ src/Main.java
@@ -10,29 +10,25 @@ import java.util.TreeMap;
 
 import javax.management.RuntimeErrorException;
 
+///// ===== Algorithmic Methods of Data Mining ===== /////
+///// ===== FINAL PROJECT ===== /////
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		final FileReader dataFile = new FileReader("tweets_15m.txt");
 		final FileReader dataFile2 = new FileReader("tweets_15m.txt");
-		//System.out.println(new File("./tweets_15m.txt").getCanonicalPath());
-		//final Reader r = new Reader(dataFile);
+
 		final LineNumberReader reader = new LineNumberReader(dataFile);
 		final LineNumberReader reader2 = new LineNumberReader(dataFile2);
-		//final BufferedReader reader = new BufferedReader(dataFile);
 		
 		HashMap<String, Valor> mapa = mapcreator(reader);
 		
-		//System.out.println(reader.getLineNumber());
-		//System.out.println(reader.markSupported());
+		//===Task 1===//
 		ArrayList<Valor> sortByNumberOfTweets = task1(mapa,reader);
-		task2(reader2,  sortByNumberOfTweets, 1);
 		
-		String[] x ={"a","admit","been","c","case","co","confirm","d","ebola","ha","hospit","howard","http","lik","not","of","paty","symptom","t","to","tvnl","u","viru","with","ww","zl"};
-		String[] y ={"airport","bathroom","clean","door","ebola","ebolaansw","ground","handl","left","of","on","paty","ppl","reider","ronanfarrow","sweat","the","think","toilet","who"};
-		String[] z ={"asnowros","at","c","co","d","ebola","hospit","howard","http","in","lik","paty","rt","symptom","t","time","treat","univer","vmwjjgucrl","washington","with"};
-		System.out.println(angle(x, z, new ArrayList<String>()));
-		//angle=1.031476373310863		 
+		//===Task 2===//
+		task2(reader2,  sortByNumberOfTweets, 500);
 				 
 		//for (int i =0; i< subSpace.size();i++)
 		//	System.out.println(subSpace.get(i));
@@ -44,7 +40,7 @@ public class Main {
 
 	public static HashMap<String, Valor> mapcreator(BufferedReader reader) throws IOException{
 		HashMap<String, Valor> mapa = new HashMap<String, Valor>();
-		for(int i = 0;i<5000;i++){
+		for(int i = 0;i<1000;i++){
 			String[] token = reader.readLine().split("\\s+");
 			for(int j=0;j<token.length;j++){
 				mapa.put(token[j], new Valor(new String(token[j])));
@@ -53,64 +49,26 @@ public class Main {
 		return mapa;
 	}
 	
-	public static class Valor {
-		final String text;
-		int numberOfTweets;
-		int aux;
-		public Valor(String text) {
-			this.text = text;
-			this.numberOfTweets = 0;
-			this.aux = -1;
-		}
-	}
-	
-	public static void task2(LineNumberReader reader, ArrayList<Valor> sortByNumberOfTweets, int Q) throws IOException{
-		int D = 100*2;
-		double angle = Math.PI/2;
-		int index = -1;
-		ArrayList<String> subSpace = subspace("frequent", D, sortByNumberOfTweets);
-		//if(Q>1000) throw new RuntimeException();
-		String stringLine = reader.readLine();
-		System.out.println(stringLine);
-		
-		String[] tokensX = stringLine.split("\\s+");
-		for(int i=1; i<1000;i++) reader.readLine();
-		//reader.setLineNumber(1000);
-		
-		for(int ĺine = 0; ĺine<5000; ĺine++){
-			stringLine = reader.readLine();
-			String[]  tokensY = stringLine.split("\\s+");
-			double aux = angle(tokensX, tokensY, subSpace);
-			if(aux<angle){
-				angle = aux;
-				//index = ĺine + 1000;
-				index = reader.getLineNumber();//ĺine + 1000;
-				System.out.println(stringLine);
-				System.out.println("index=" + index + "  angle=" + angle);
-				//textAUX = stringLine;
-			}
-			
-		}
-	}
+/// ===== TASK 1 ===== ///	
 	public static ArrayList<Valor> task1(HashMap mapa, BufferedReader reader) throws IOException{
 		String stringLine = reader.readLine();
 		String[] token = stringLine.split("\\s+");
-		for(int ĺine = 0;  (stringLine = reader.readLine()) != null && ĺine<5000; ĺine++){
+		for(int line = 0;  (stringLine = reader.readLine()) != null && line<1000; line++){
 			for(int j=0;j<token.length;j++){
 				Valor obj = (Valor) mapa.get(token[j]);
 				if(obj != null){
-					if(obj.aux != ĺine){
-						obj.aux=ĺine;
+					if(obj.aux != line){
+						obj.aux=line;
 						obj.numberOfTweets++;
 					}
 				}
 			}
 			token = stringLine.split("\\s+");
 		}
-		
+
 		ArrayList<Valor> sortByNumberOfTweets = new ArrayList<Valor>();
 		sortByNumberOfTweets.addAll(mapa.values());
-		
+
 		Collections.sort(sortByNumberOfTweets, new Comparator<Valor>() {
 			@Override
 			public int compare(Valor o1, Valor o2){
@@ -122,31 +80,87 @@ public class Main {
 					return -1;
 			}
 		});
-		
 		return sortByNumberOfTweets;
 	}
-	
-	public static double angle(String[] x, String[] y, ArrayList<String> subspace){
+
+/// ===== TASK 2 ===== ///	
+	public static void task2(LineNumberReader reader, ArrayList<Valor> sortByNumberOfTweets, int Q) throws IOException{
+		long startTime = System.currentTimeMillis();
+		
+		if(Q>1000 || Q<1) throw new RuntimeException();
+		
+		double angle = Math.PI/2;
+		int index = -1;
+		String stringLine = null;
+		String twt = null;
+		
+		for(int i=1;i<=Q;i++) stringLine = reader.readLine();
+		String[] tokensX = stringLine.split("\\s+");
+		System.out.println("Querry: " + stringLine);
+		
+		for(int j=Q; j<1000;j++) reader.readLine();
+		
+		for(int line = 0; line<5000; line++){
+			stringLine = reader.readLine();
+			String[]  tokensY = stringLine.split("\\s+");
+			double aux = angle(tokensX, tokensY);
+			if(aux<angle){
+				angle = aux;
+				index = reader.getLineNumber();
+				twt = stringLine;	
+			}
+		}
+		
+		System.out.println("Nearest: " + twt );
+		System.out.println("Index of Nearest=" + index + "    Angle=" + angle);
+		
+		long endTime   = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+		System.out.println("Time elapsed: " + totalTime + "ms");
+		
+		//int D = 100*2;
+		//ArrayList<String> subSpace = subspace("frequent", D, sortByNumberOfTweets);
+	}
+
+
+// ===== Angle ===== //
+	public static double angle(String[] x, String[] y){
 		int cont = 0;
 		for(int i = 0; i< x.length; i++)
 			for(int j = 0; j< y.length; j++)
 				if(x[i].equals(y[j]))
 					//if(subspace.contains(x[i])) //have to be in the subspace
-						cont++;
+					cont++;
 		return Math.acos(cont/(Math.sqrt(x.length)*Math.sqrt(y.length)));
 	}
-	
+
+// ===== Generate Subspaces ===== //
 	public static ArrayList<String> subspace(String f, int d, ArrayList<Valor> sortByNumberOfTweets){
 		ArrayList<String> ret = new ArrayList<String>();
 		if(f.equals("frequent")){
-			for(int i =0; i<d; i++)
+			for(int i=0; i<d; i++)
 				ret.add(sortByNumberOfTweets.get(i).text);
-		}else if(f.equals("infrequent")){
+		}
+		else if(f.equals("infrequent")){
 			for(int i =0; i<d; i++)
 				ret.add(sortByNumberOfTweets.get(sortByNumberOfTweets.size()-i-1).text);
-		}else if(f.equals("random")){
+		}
+		else if(f.equals("random")){
 			ret.add("TODO");//TODO
-		}else throw new RuntimeException();
+		}
+		else throw new RuntimeException();
 		return ret;
+	}
+
+// ===== Class Angle ===== //
+	public static class Valor {
+		final String text;
+		int numberOfTweets;
+		int aux;
+		public Valor(String text) {
+			this.text = text;
+			this.numberOfTweets = 0;
+			this.aux = -1;
+		}
 	}
 }
